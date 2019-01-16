5f9348e7c1dc51990bc6739cdfef4b0ca663931d
Ilya Smelkov <ismelkov@restream.rt.ru>
2019-01-16 18:58:52 +0300

    Nav construction 🍟

---
+++ src/models/Construction.ts
@@ -0,0 +1,69 @@
+import { Point } from "../lib/Point"
+import { ConstructionTile } from "./Ship"
+export abstract class Construction {
+  constructor(public pos: Point) {}
+  abstract get size(): Point
+  public abstract tiles: { [key: string]: ConstructionTile }
+
+  public isIntersectional({ x, y }: Point): boolean {
+    return (
+      x >= this.pos.x &&
+      x < this.pos.x + this.size.x &&
+      y >= this.pos.y &&
+      y < this.pos.y + this.size.y
+    )
+  }
+
+  public tileAt({ x, y }: Point): ConstructionTile {
+    return (
+      this.tiles[[x - this.pos.x, y - this.pos.y].join(" ")] ||
+      new ConstructionTile(false, "E")
+    )
+  }
+}
+
+export class DoorSystem extends Construction {
+  public tiles: { [key: string]: ConstructionTile } = {
+    "0 0": new ConstructionTile(false, "╔"),
+    "1 0": new ConstructionTile(false, "╕"),
+    "0 1": new ConstructionTile(false, "╙"),
+    "1 1": new ConstructionTile(true, "∅")
+  }
+
+  get size(): Point {
+    return new Point(2, 2)
+  }
+}
+
+export class MissileSystemConstruction extends Construction {
+  public tiles: { [key: string]: ConstructionTile } = {
+    "0 0": new ConstructionTile(false, "╔"),
+    "1 0": new ConstructionTile(false, "╤"),
+    "2 0": new ConstructionTile(false, "╗"),
+    "0 1": new ConstructionTile(false, "╟"),
+    "1 1": new ConstructionTile(true, "∅"),
+    "2 1": new ConstructionTile(false, "╢"),
+    "0 2": new ConstructionTile(false, "⊑"),
+    "1 2": new ConstructionTile(true, " "),
+    "2 2": new ConstructionTile(false, "⊒")
+  }
+
+  get size(): Point {
+    return new Point(3, 3)
+  }
+}
+
+export class NavigationSystem extends Construction {
+  public tiles: { [key: string]: ConstructionTile } = {
+    "0 0": new ConstructionTile(false, "⇖"),
+    "1 0": new ConstructionTile(false, "⇗"),
+    "0 1": new ConstructionTile(false, "⇅"),
+    "1 1": new ConstructionTile(false, "⇅"),
+    "0 2": new ConstructionTile(false, "╙"),
+    "1 2": new ConstructionTile(true, "∅")
+  }
+
+  get size(): Point {
+    return new Point(2, 3)
+  }
+}
--- src/models/Ship.ts
+++ src/models/Ship.ts
@@ -11,6 +11,12 @@ import { Input, IdleInput } from "../inputs/Input"
 import { Point } from "../lib/Point"
 import { Direction } from "../lib/Direction"
 import { LaborType } from "../commands/Labor"
+import {
+  Construction,
+  DoorSystem,
+  MissileSystemConstruction,
+  NavigationSystem
+} from "./Construction"
 
 export abstract class TileVisitor<T> {
   public abstract visitConstruction(construction: ConstructionTile): T
@@ -261,69 +267,6 @@ const findPath = function(
   return
 }
 
-export abstract class Construction {
-  constructor(public pos: Point) {}
-
-  abstract get size(): Point
-  public abstract tileAt({ x, y }: Point): ConstructionTile
-
-  public isIntersectional({ x, y }: Point): boolean {
-    return (
-      x >= this.pos.x &&
-      x < this.pos.x + this.size.x &&
-      y >= this.pos.y &&
-      y < this.pos.y + this.size.y
-    )
-  }
-}
-
-export class DoorSystem extends Construction {
-  get size(): Point {
-    return new Point(2, 2)
-  }
-
-  public tileAt({ x, y }: Point): ConstructionTile {
-    switch ([x - this.pos.x, y - this.pos.y].join(" ")) {
-      case "0 0":
-        return new ConstructionTile(false, "╔")
-      case "0 1":
-        return new ConstructionTile(false, "╙")
-      case "1 0":
-        return new ConstructionTile(false, "╕")
-      case "1 1":
-        return new ConstructionTile(true, "∅")
-      default:
-        return new ConstructionTile(false, "E")
-    }
-  }
-}
-
-export class MissileSystemConstruction extends Construction {
-  public static tiles: { [key: string]: ConstructionTile } = {
-    "0 0": new ConstructionTile(false, "╔"),
-    "1 0": new ConstructionTile(false, "╤"),
-    "2 0": new ConstructionTile(false, "╗"),
-    "0 1": new ConstructionTile(false, "╟"),
-    "1 1": new ConstructionTile(true, "∅"),
-    "2 1": new ConstructionTile(false, "╢"),
-    "0 2": new ConstructionTile(false, "⊑"),
-    "1 2": new ConstructionTile(true, " "),
-    "2 2": new ConstructionTile(false, "⊒")
-  }
-
-  get size(): Point {
-    return new Point(3, 3)
-  }
-
-  public tileAt({ x, y }: Point): ConstructionTile {
-    return (
-      MissileSystemConstruction.tiles[
-        [x - this.pos.x, y - this.pos.y].join(" ")
-      ] || new ConstructionTile(false, "E")
-    )
-  }
-}
-
 export class Ship {
   public creatures: Creature[] = []
   public constructions: Construction[] = []
@@ -334,8 +277,9 @@ export class Ship {
     public width: number,
     public height: number
   ) {
-    this.creatures = [new Creature(0, new Point(10, 1))]
+    this.creatures = [new Creature(0, new Point(10, 15))]
     this.constructions = [
+      new NavigationSystem(new Point(10, 1)),
       new DoorSystem(new Point(5, 13)),
       new MissileSystemConstruction(new Point(6, 18))
     ]
